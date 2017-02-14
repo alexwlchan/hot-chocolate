0e155493d0c012c25c91f850deee9ab7c2a0f3cc
Alex Chan <alex@alexwlchan.net>
2017-02-14 08:33:37 +0000

    Read multiple posts from a site folder

--- src/cacao/__init__.py
+++ src/cacao/__init__.py
@@ -1,7 +1,12 @@
 # -*- encoding: utf-8
 
+import os
+
 from jinja2 import Environment, PackageLoader, select_autoescape
 import markdown
+
+
+MARKDOWN_EXTENSIONS = ('.txt', '.md', '.mdown', '.markdown')
 
 
 class Site:
@@ -10,6 +15,19 @@ class Site:
     """
     def __init__(self, language=None):
         self.language = language or 'en'
+        self.posts = []
+
+    @classmethod
+    def from_folder(cls, path):
+        """
+        Construct a ``Site`` instance from a folder on disk.
+        """
+        s = cls()
+        for root, _, filenames in os.walk(os.path.join(path, 'posts')):
+            for f in filenames:
+                if os.path.splitext(f)[1].lower() in MARKDOWN_EXTENSIONS:
+                    s.posts.append(Article.from_file(os.path.join(root, f)))
+        return s
 
 
 class Article:
@@ -19,6 +37,9 @@ class Article:
     def __init__(self, content, path=None):
         self.content = content
         self.path=path
+
+    def __repr__(self):
+        return '<%s path=%r>' % (type(self).__name__, self.path)
 
     @classmethod
     def from_file(cls, path):
@@ -36,8 +57,8 @@ def main():
         loader=PackageLoader('cacao', 'templates'),
         autoescape=select_autoescape(['html'])
     )
-    site = Site()
-    template = env.get_template('article.html')
-    article = Article.from_file('example.md')
-    with open('example.html', 'w') as f:
-        f.write(template.render(site=site, article=article))
+    site = Site.from_folder('content')
+    for i, post in enumerate(site.posts):
+        template = env.get_template('article.html')
+        with open('example_%d.html' % i, 'w') as f:
+            f.write(template.render(site=site, article=post))
