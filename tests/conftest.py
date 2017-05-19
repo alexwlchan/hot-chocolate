b6e1db8dfcecaa2f513f86eb0f18be009ea54fe3
Alex Chan <alex@alexwlchan.net>
2017-05-19 23:14:53 +0100

    Get the tests working again (ish)

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,8 +6,8 @@ from hotchocolate import Site
 
 
 # TODO: Tidy this up, and don't duplicate code from cli.py
-curdir = os.path.abspath(os.curdir)
-os.chdir('tests/examplesite')
-site = Site.from_folder('content')
-site.build()
-os.chdir(curdir)
+# curdir = os.path.abspath(os.curdir)
+# os.chdir('tests/examplesite')
+# site = Site.from_folder('content')
+# site.build()
+# os.chdir(curdir)
--- tests/test_pagination.py
+++ tests/test_pagination.py
@@ -15,26 +15,26 @@ POSTS = ['post_%d' % i for i in range(1, 21)]
     (0, {
         'slug': '/',
         'articles': POSTS[:5],
-        'next': '/page/2',
+        'next': '/page/2/',
         'prev': None,
     }),
     (1, {
-        'slug': '/page/2',
+        'slug': '/page/2/',
         'articles': POSTS[5:10],
-        'next': '/page/3',
+        'next': '/page/3/',
         'prev': '/',
     }),
     (2, {
-        'slug': '/page/3',
+        'slug': '/page/3/',
         'articles': POSTS[10:15],
-        'next': '/page/4',
-        'prev': '/page/2',
+        'next': '/page/4/',
+        'prev': '/page/2/',
     }),
     (3, {
-        'slug': '/page/4',
+        'slug': '/page/4/',
         'articles': POSTS[15:20],
         'next': None,
-        'prev': '/page/3',
+        'prev': '/page/3/',
     }),
 ])
 def test_index_page_pagination(idx, pageset):
@@ -50,26 +50,26 @@ def test_index_page_pagination(idx, pageset):
     (0, {
         'slug': '/tag/',
         'articles': POSTS[:5],
-        'next': '/tag/2',
+        'next': '/tag/2/',
         'prev': None,
     }),
     (1, {
-        'slug': '/tag/2',
+        'slug': '/tag/2/',
         'articles': POSTS[5:10],
-        'next': '/tag/3',
+        'next': '/tag/3/',
         'prev': '/tag/',
     }),
     (2, {
-        'slug': '/tag/3',
+        'slug': '/tag/3/',
         'articles': POSTS[10:15],
-        'next': '/tag/4',
-        'prev': '/tag/2',
+        'next': '/tag/4/',
+        'prev': '/tag/2/',
     }),
     (3, {
-        'slug': '/tag/4',
+        'slug': '/tag/4/',
         'articles': POSTS[15:20],
         'next': None,
-        'prev': '/tag/3',
+        'prev': '/tag/3/',
     }),
 ])
 def test_tag_page_pagination(idx, pageset):
