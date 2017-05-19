69c66b3c2657e35dc4e6d285f7e60993268bc76b
Alex Chan <alex@alexwlchan.net>
2017-05-19 23:09:02 +0100

    Only support Python 3.6

--- setup.py
+++ setup.py
@@ -29,8 +29,6 @@ setup(
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Other Audience',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3 :: Only',
     ],
