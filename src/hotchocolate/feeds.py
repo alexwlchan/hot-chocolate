892b421fc3b6179350a47160d8d8d13f838dbb56
Alex Chan <a.chan@wellcome.ac.uk>
2017-05-24 18:18:50 +0100

    Render an Atom feed

--- src/hotchocolate/feeds.py
+++ src/hotchocolate/feeds.py
@@ -7,11 +7,34 @@ and JSON Feed.  All three feed types are build for sites using Hot Chocolate.
 
 """
 
+from datetime import datetime
 import os
+from urllib.parse import urlparse
 
 import jinja2
 
-from . import templates
+from . import templates, utils
+
+
+# How many posts to include in the feeds.
+MAX_POSTS = 20
+
+
+def get_tag_uri(url, date):
+    """
+    Creates a TagURI.
+    See http://web.archive.org/web/20110514113830/http://diveintomark.org/archives/2004/05/28/howto-atom-id
+    """
+    # Taken from feedgenerator.django.utils.feedgenerator
+    bits = urlparse(url)
+    d = ''
+    if date is not None:
+        d = ',%s' % date.strftime('%Y-%m-%d')
+    fragment = ''
+    if bits.fragment != '':
+        fragment = '/%s' % (bits.fragment)
+    return 'tag:%s%s:%s%s' % (bits.hostname, d, bits.path, fragment)
+
 
 
 def get_feed_template(name):
@@ -27,3 +50,21 @@ def get_feed_template(name):
             os.path.dirname(os.path.abspath(__file__)), 'templates'
         ))
         return env.get_template(name)
+
+
+def build_atom_feed(site, posts):
+    """
+    Given a list of posts, return a rendered Atom feed.
+    """
+    template = get_feed_template('atom.xml')
+    for post in posts:
+        post.metadata['tag_uri'] = get_tag_uri(
+            url=site.settings['url'] + post.metadata['slug'],
+            date=post.metadata['date']
+        )
+    atom_xml = template.render(
+        site=site,
+        posts=posts[:MAX_POSTS],
+        updated_date=str(datetime.now())
+    )
+    return utils.minify_xml(atom_xml.encode('utf8'))
