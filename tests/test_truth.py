717b33cb6c41fc832df88fe1d76f1d6ce4bb7687
Alex Chan <alex@alexwlchan.net>
2024-01-24 00:29:57 +0000

    Add a couple of missing types

--- tests/test_truth.py
+++ tests/test_truth.py
@@ -1,9 +1,9 @@
 from hotchocolate import __version__
 
 
-def test_version():
+def test_version() -> None:
     assert __version__ == "2023.1"
 
 
-def test_truth():
+def test_truth() -> None:
     assert True
